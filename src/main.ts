f88ec3e5acd0f34d8078951cf394a6e04ed91f57
Harsh Kurulkar <harshkurulkar7@gmail.com>
2023-07-19 11:29:30 +0530

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule , DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('email')
+  .setDescription('send email details')
+  .setVersion('1.0')
+  .addTag('contact')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/contact', app, document);
+
+
   await app.listen(8005);
 }
 bootstrap();
